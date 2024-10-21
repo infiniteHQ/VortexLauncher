23470b4f2e79cf0e0ea10aa625ad5c8f88ecf842
diegomrno <diegomoreno.pro45@gmail.com>
2024-10-21 23:43:49 +0200

    fix: Build/manifest

--- build/build.sh
+++ build/build.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
+# Create build folders
 mkdir -p build_spdlog
 mkdir -p build
 
+# Compile stack
 cd build_spdlog && cmake ../../lib/spdlog && make -j$(nproc) install
-
 cd ../build && cmake ../.. && make -j$(nproc) install
-
 cd ..
 
-cp ./build/libvortex_launcher_shared.so ./build/bin/
+# Cpy 
+cp ../manifest.json dist/
+cp ../LICENSE dist/
 
 mkdir -p shipping/linux
+mkdir -p shipping/windows
+mkdir -p shipping/macos
+
 tar -cvzf ./shipping/linux/vortex_launcher_1.2.tar.gz dist/
 cd shipping/linux
 
