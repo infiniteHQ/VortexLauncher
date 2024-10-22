26b725710d382bb729e2b015fdad76b6abb89a9e
diegomrno <diegomoreno.pro45@gmail.com>
2024-10-22 17:25:29 +0200

    feat: Embed installer & utils

--- build/build.sh
+++ build/build.sh
@@ -7,11 +7,18 @@ mkdir -p build
 # Compile stack
 cd build_spdlog && cmake ../../lib/spdlog && make -j$(nproc) install
 cd ../build && cmake ../.. && make -j$(nproc) install
+
 cd ..
 
 # Cpy 
 cp ../manifest.json dist/
 cp ../LICENSE dist/
+
+# Build embedded installer/updater/uninstaller
+cd ../lib/installer/build
+sudo bash build.sh
+cd ../../../build
+cp -rn ../lib/installer/build/build/bin/* dist/bin/
 
 mkdir -p shipping/linux
 mkdir -p shipping/windows
