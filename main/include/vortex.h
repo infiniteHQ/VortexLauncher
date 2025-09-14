38bec387778db6ff2f55bbf69f2904c4ce2359fc
diegomrno <diegomoreno.pro45@gmail.com>
2025-09-14 17:20:52 +0200

    update: version num

--- main/include/vortex.h
+++ main/include/vortex.h
@@ -8,8 +8,8 @@
 // Versions & Build identification
 //_____________________________________________________________________________
 // (Integer encoded as XYYZZ for use in #if preprocessor conditionals, e.g. '#if VORTEXLAUNCHER_VERSION_NUM >= 1.5.0')
-#define VORTEXLAUNCHER_VERSION     "1.5.1"
-#define VORTEXLAUNCHER_VERSION_NUM 010501  // Major.Medium.Minor
+#define VORTEXLAUNCHER_VERSION     "1.5.2"
+#define VORTEXLAUNCHER_VERSION_NUM 010502  // Major.Medium.Minor
 
 //_____________________________________________________________________________
 
