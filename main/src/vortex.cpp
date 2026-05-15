51c398555b67514631f2eb0e7770c8d4a9554332
diegomrno <d@infinite.si>
2026-05-15 10:52:16 +0200

    refacto: Cleanings

--- main/src/vortex.cpp
+++ main/src/vortex.cpp
@@ -308,6 +308,7 @@ VORTEX_API bool VortexMaker::CheckIfProjectRunning(const std::string &path) {
   return false;  // Project is not running
 }
 
+// TODO move this to vortex_utils (in Vortex Editor), that will be triggered by the vx.bat/vx.sh starting script
 void writeSessionEndState(const std::string &session_id, const std::string &state) {
   // Set the session directory path depending on the platform
   std::string session_dir;
@@ -464,33 +465,6 @@ VORTEX_API void VortexMaker::OpenProject(const std::string &path, const std::str
 
   VortexMaker::executeInChildProcess(command);
   return;
-
-  /*
-    std::string target_path = VortexMaker::getHomeDirectory() + "/.vx/sessions/" + session_id + "/crash/core_dumped.txt";
-    std::string crash_script_command;
-
-    if (VortexMaker::IsWindows()) {
-      crash_script_command = "cmd.exe /C \"cd \"" + project_path + "\" && \"" + vortex_path + "\\bin\\handle_crash.bat\" \""
-    + target_path + "\"; & \"" + vortex_path + "\\bin\\vortex.exe\" --editor --session_id=" + "\"" + session_id + "\"" + "; &
-    \"" + vortex_path + "\\bin\\vortex.exe\" -crash --session_id=" + "\"" + session_id + "\"" + " ; & \"" + vortex_path +
-                             "\\bin\\vortex_utils.exe\" -rms --session_id=" + "\"" + session_id + "\"" + "\"";
-    } else {
-      crash_script_command =
-          "cd \"" + project_path + "\" && bash \"" + vortex_path + "/bin/handle_crash.sh\" " + target_path + " " + command;
-    }
-
-    std::cout << "Bootstrap: Starting with command: " << crash_script_command << std::endl;
-
-    if () {
-      writeSessionEndState(session_id, "success");
-    } else {
-      std::string crash_handle_command;
-      if (!VortexMaker::IsWindows()) {
-        crash_handle_command = "\"" + vortex_path + "/bin/vortex\" -crash --session_id=" + session_id;
-        VortexMaker::executeInChildProcess(crash_handle_command);
-        writeSessionEndState(session_id, "fail");
-      }
-    }*/
 }
 
 VORTEX_API void VortexMaker::RefreshActiveSessions() {
